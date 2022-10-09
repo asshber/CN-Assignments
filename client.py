7b1ae50ad70e263f2287bc458258769fd9d70214
abdn123 <77683256+abdn123@users.noreply.github.com>
2022-10-09 16:04:58 +0500

    final

--- client.py
+++ client.py
@@ -1,4 +1,5 @@
 from asyncio import windows_events
+from multiprocessing import connection
 import socket
 import threading
 from tkinter import *
@@ -6,11 +7,19 @@ from tkinter import *
 def message_send():
    # while True:
         message = Socket_chat.get("0.0", END)
+        Socket_chat.delete("0.0", END)
         sock.send(message.encode())
 def message_recv(sock):
     while True:
         message = ""
         while message.lower().strip() != 'bye':
+            global connection
+            if connection:
+                Socket_notif.delete("0.0", END)    
+                Socket_notif.insert(END, "Connected")
+            else:
+                Socket_notif.delete("0.0", END)
+                Socket_notif.insert(END, "Not Connected")
             message = sock.recv(1024).decode()
             Socket_output.delete("0.0", END)
             Socket_output.insert(END,message)
@@ -21,19 +30,16 @@ def message_recv(sock):
 def make_conn():
     data = Socket_data.get()
     data=data.split(':')
-    print(data[1])
     global sock
+    global connection
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     sock.connect((data[0],int(data[1])))
+    connection = True
     message = ""
-
     while message == "":
         message = sock.recv(1024).decode()
-
-    
     print('Received from server: ' + message)
     
-    #t1 = threading.Thread(target=message_send, args=(sock,)).start()
     t2 = threading.Thread(target=message_recv, args=(sock,)).start()      
 
 window=Tk()
@@ -49,8 +55,11 @@ Socket_send_button=Button(window,text="Send",command=message_send)
 Socket_send_button.place(x=400,y=190)
 Socket_recv_lable=Label(window,text="Recieved Message:")
 Socket_recv_lable.place(x=10,y=220)
-Socket_output=Text(window,height=5, width=53)
+Socket_output=Text(window,height=5, width=53, bg="gray")
 Socket_output.place(x=10,y=240)
+Socket_notif=Text(window, height=1.25, width=14, bg="Yellow", font =("Courier", 12), padx=13, pady=10)
+Socket_notif.insert(END, "Not Connected")
+Socket_notif.place(x=250,y=30)
 
 Socket_button=Button(window,text="Connect",command=make_conn)
 Socket_button.place(x=150,y=30)
--- server.py
+++ server.py
@@ -1,20 +1,32 @@
+from distutils.command.config import config
 import socket
 import threading
 from tkinter import *
 
 
 def message_send():
+    try:
         message = Socket_chat.get("0.0", END)
+        Socket_chat.delete("0.0", END)
         conn.send(message.encode())
+    except Exception as E:
+        print("No connection")
 
 def message_recv():
     data = ""
+    global connection
     while data.lower().strip() != 'bye':
+        if connection:
+            Socket_notif.delete("0.0", END)    
+            Socket_notif.insert(END, "Connected")
+        else:
+            Socket_notif.delete("0.0", END)
+            Socket_notif.insert(END, "Not Connected")
         data = conn.recv(1024).decode()
         Socket_output.delete("0.0", END)
         Socket_output.insert(END,data)
-        print(data)
-    #conn.close()
+        print(data)    
+        #conn.close()
 
 def start_service():
     port = Socket_data.get()
@@ -33,8 +45,7 @@ def start_service():
         print("Port Error "+ str(E))
 
     print(f"Listening as {host}:{port}")
-    connection = False
-    #while True:
+    global connection
     while connection == False:
         global conn
         conn, addr = sock.accept()
@@ -42,30 +53,14 @@ def start_service():
             print("Connected by: ", addr)
             conn.send(("Connection estabilished!").encode())
             connection = True
-    
-    #t1 = threading.Thread(target=message_send, args=(conn,)).start()
     t2 = threading.Thread(target=message_recv, args=()).start()
-        
-        # msg = input("You: ")
-        # conn.send(msg.encode())
-        # if msg == "q":
-        #     break
-
-        # data = ""
-        # while data == "":
-        #     data = (conn.recv(2048)).decode()
-        # if data == "q":
-        #     conn.close()
-        #     break
-        # else:
-        #     print("Server: ", data)
-        #     data = ""
 
 
+connection = False
 window=Tk()
-Socket_lable=Label(window,text="Enter  Port Number:")
+Socket_lable=Label(window,text="Enter Port Number:")
 Socket_lable.place(x=10,y=10)
-Socket_data=Entry(window,text="",bd=5)
+Socket_data=Entry(window,text="Port",bd=5)
 Socket_data.place(x=10,y=30)
 Socket_send_lable=Label(window,text="Type here to send Message:")
 Socket_send_lable.place(x=10,y=80)
@@ -75,10 +70,13 @@ Socket_send_button=Button(window,text="Send",command=message_send)
 Socket_send_button.place(x=400,y=190)
 Socket_recv_lable=Label(window,text="Recieved Message:")
 Socket_recv_lable.place(x=10,y=220)
-Socket_output=Text(window,height=5, width=53)
+Socket_output=Text(window,height=5, width=53, bg="gray")
 Socket_output.place(x=10,y=240)
+Socket_notif=Text(window, height=1.25, width=14, bg="Yellow", font =("Courier", 12), padx=13, pady=10)
+Socket_notif.insert(END, "Not Connected")
+Socket_notif.place(x=250,y=30)
 
-Socket_button=Button(window,text="Connect",command=start_service)
+Socket_button=Button(window,text="Start Listening",command=start_service)
 Socket_button.place(x=150,y=30)
 window.title('Socket Assignment - Server')
 window.geometry("450x400+10+20")
