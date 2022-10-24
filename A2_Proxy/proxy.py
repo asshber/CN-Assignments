8f863623f91a838567b07ba96936588b43420356
abdn123 <77683256+abdn123@users.noreply.github.com>
2022-10-25 03:05:28 +0500

    Update proxy.py

--- A2_Proxy/proxy.py
+++ A2_Proxy/proxy.py
@@ -9,9 +9,11 @@ import ssl
 
 
 max_connections = 5
-BUFFER_SIZE = 4096
+BUFFER_SIZE = 1048576
 CACHE_DIR = "./cache"
 NO_OF_OCC_FOR_CACHE = 2
+context = ssl.SSLContext(ssl.PROTOCOL_TLS_SERVER)
+black_listed = []
 
 def parse(conn):
         req = conn.recv(BUFFER_SIZE)
@@ -38,40 +40,61 @@ def parse(conn):
                         req_port = 80
                     elif(method == b'CONNECT'):
                         req_port=443
-                print(method)
-                print(url_str)
-                print(req_port)
-                if method == b'GET':
-                    proxy(url_str, req_port, conn, req)
-                elif method ==b'CONNECT':
-                    https_proxy(url_str, req_port, conn,req)
+                print("Method: ", method)
+                print("URL: ", url_str)
+                print("Port: ", req_port)
+                if url_str.decode() in black_listed:
+                    print("Blacklisted Website...")
+                    return
+                else:
+                    if method == b'GET':
+                        proxy(url_str, req_port, conn, req)
+                    elif method ==b'CONNECT':
+                        https_proxy(url_str, req_port, conn,req)
 
 def https_proxy(webserver, port, conn, req):
-    #print(req)
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    try: 
+        s = context.wrap_socket(s, server_side=True, do_handshake_on_connect=False)
+        if s:
+            raise Exception('')
+        s.do_handshake()
+    except:
+        s.close()
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     filename=webserver
     webserver=webserver.split(b'/')
     webserver=webserver[0]
-    webserver=str(webserver,'utf-8')
-    os.chdir('/home/asshber/CN-Assignments/A2_Proxy/cache')
+    os.chdir(r'C:\Users\hp\CN-Assignments\A2_Proxy\cache')
     s.connect((webserver, int(port)))
     #Connect ka reply bhejna hy yaha client ko
-    s = ssl.wrap_socket(s, keyfile=None, certfile=None, server_side=False, cert_reqs=ssl.CERT_NONE, ssl_version=ssl.PROTOCOL_SSLv23)
+
     filename=str(filename)
     filename=filename.replace('/','')
-    string=bytes(f"GET / HTTP/1.1\r\nHost: {webserver}\r\n\r\n",encoding="utf-8")
-    s.sendall(string)
+    conn.sendall(b'HTTP/1.1 200 Connection Estabilished\r\n\r\n')
+    # string=bytes(f"GET / HTTP/1.1\r\nHost: {webserver}\r\n\r\n",encoding="utf-8")
+    # s.sendall(string)
     while True:
-        new = s.recv(4096)
-        if not new:
-            s.close()
-            break
-        print(new)
-        conn.send(new)  
+        try:
+            request = conn.recv(BUFFER_SIZE)
+            if (request != b''):
+                s.sendall(request)
+                print("request: ", request)
+        except error as e:
+            pass
+        try:
+            response = s.recv(BUFFER_SIZE)
+            if (response != b''):
+                conn.sendall(response)
+                print("response: ", response)
+        except error as e:
+            pass
+    s.close()
 
 def initialize_proxy():
     p_socket=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-    p_socket.bind(('127.0.0.1',port))
+    p_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    p_socket.bind(('', port))
     p_socket.listen(max_connections)
     print("Listening...")
 
@@ -81,43 +104,13 @@ def initialize_proxy():
         #t1=threading.Thread(target=parse,args=(conn))
         print("Connection Request from: " + str(addr[0]) + ", port: " + str(addr[1]))
         start_new_thread(parse, (conn,))
-        #t1.start()
-        #New fun will be from here threading will also be applied when calling
-        # req = conn.recv(BUFFER_SIZE)
-        # #print(req)
-        # arr=req.split(b'\n')
-        # #print(arr)
-        # for i in range(0,len(arr)-2):
-        #     if (i==0):
-        #         method_url=arr[i].split(b' ')
-        #         method=method_url[0]
-        #         url=method_url[1].split(b'://')
-        #         #print(url)
-        #         url_str=url[0]
-        #         if url_str == b'http':
-        #             url_str=url[1]
-        #         if b':' in url_str:
-        #             req_port=url_str.split(b':')
-        #             url_str=req_port[0]
-        #             req_port=req_port[1]
-        #         else:
-        #             if(method==b'GET'):
-        #                 # print(url_str)
-        #                 # http_proxy(url_str, 80, conn, req)
-        #                 req_port = 80
-        #             elif(method == b'CONNECT'):
-        #                 req_port=443
-        #         print(method)
-        #         print(url_str)
-        #         print(req_port)
-        #         proxy(url_str, req_port, conn, req)
-            
+        
 def proxy(webserver, port, conn, req):
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     filename=webserver
     webserver=webserver.split(b'/')
     webserver=webserver[0]
-    os.chdir('/home/asshber/CN-Assignments/A2_Proxy/cache')
+    os.chdir(r'C:\Users\hp\CN-Assignments\A2_Proxy\cache')
     sock.connect((webserver, int(port)))
     filename=str(filename)
     filename=filename.replace('/','')
@@ -152,7 +145,11 @@ if __name__== "__main__":
 
     port=int(arg.p)
 
-    print(port)
+
+    with open('blacklisted.txt', 'r') as file:
+        for line in file:
+            black_listed.append(line.rstrip())
+    print(black_listed)
 
     if not os.path.isdir(CACHE_DIR):
         os.makedirs(CACHE_DIR)  #All the cached file will go here
